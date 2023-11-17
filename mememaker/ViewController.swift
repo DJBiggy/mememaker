e117a636de5584657f6ffd26cdc1b21e18989de6
Juanathan Bean <randot771@gmail.com>
2023-11-17 13:46:10 -0500

    s

--- mememaker/ViewController.swift
+++ mememaker/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    struct CaptionOption
+    {
+        var emoji: String
+        var caption:String
+    }
+    @IBOutlet var tSC: UISegmentedControl!
+    @IBOutlet var tCL: UILabel!
+    
+    @IBOutlet var bSC: UISegmentedControl!
+    @IBOutlet var bCL: UILabel!
+    
+    
+    
+    var top = [CaptionOption(emoji: <#T##String#>, caption: ```` `)]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    
+    
+    
 }
 
